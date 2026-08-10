fc6c78a4d2b75847ad2b70ccb41f9cb35cdcaa34
Anupam Mediratta <mediratta@gmail.com>
2026-08-10 18:55:28 +0530

    Merge pull request #177 from anupamme/fix-repo-node-php-runner-command-injection-child-process

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const path = require('path')
-const { execSync } = require('child_process')
+const { execFileSync } = require('child_process')
 const settings = {}
 settings.disableRegisterGlobalModel = false
 
 function run (script) {
+  const scriptPath = path.join(__dirname, script)
   try {
-    const stdout = execSync(`php ${path.join(__dirname, script)}`)
+    const stdout = execFileSync('php', [scriptPath])
     return stdout.toString()
   } catch (err) {
     throw new Error(`PHP process exited with code ${err.status}`)
@@ -27,7 +28,7 @@ function runWithData (template, model) {
   const jsonModel = JSON.stringify(model, circular())
 
   try {
-    const stdout = execSync(`php ${path.join(__dirname, '/loader.php')}`, {
+    const stdout = execFileSync('php', [path.join(__dirname, '/loader.php')], {
       input: jsonModel
     })
     return stdout.toString()
